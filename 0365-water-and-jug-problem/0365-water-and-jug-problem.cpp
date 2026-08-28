bcaa39553e93f2cdbef6e3035963dc2feb8d45f6
Kavin Senthil <kavinsenthiloff24@gmail.com>
2026-08-28 15:17:00 +0530

    Time: 0 ms (100%), Space: 7.7 MB (92.75%) - LeetHub

--- 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
+    int gc(int a,int b){
+        if(b==0)return a;
+        return gc(b,a%b);
+    }
     bool canMeasureWater(int x, int y, int target) {
         if(target==0)return true;
         if(target > x + y)return false;
-        int g= __gcd(x,y);
+        int g= gc(x,y);
         return (target%g==0);
     }
 };
